323e867cfee8c0b97667ef1eea8750238bbb4d90
Daichi <a5822108@aoyama.jp>
2025-01-19 17:55:03 +0900

    5: textureshaderの追加

--- 2D-StableFluids/textureshader.fs
+++ 2D-StableFluids/textureshader.fs
@@ -4,5 +4,18 @@
 //
 //  Created by 堀田大智 on 2025/01/18.
 //
+#version 330 core
 
+// 出力データに関する宣言
+// out vec4 color;
+in vec3 Color;          // 頂点シェーダーから渡される頂点ごとの色データ
+in vec2 Texcoord;       // 頂点シェーダーから渡される頂点ごとの座標データ
 
+out vec4 FragColor;     // フラグメントシェーダーから出力される最終的なピクセルの色
+
+uniform sampler2D tex;  // 使用するテクスチャを指定するためのユニフォーム変数
+
+void main(){
+    // テクスチャの色を取得し、アルファ値を含むベクトルと乗算して最終色を決定
+    FragColor = texture(tex, Texcoord) * vec4(1.0, 1.0, 1.0, 1.0);
+}
--- 2D-StableFluids/textureshader.vs
+++ 2D-StableFluids/textureshader.vs
@@ -4,4 +4,17 @@
 //
 //  Created by 堀田大智 on 2025/01/18.
 //
+#version 330 core
 
+// 頂点データの入力
+layout(location = 0) in vec3 vertexPosition_modelspace; // モデル空間での頂点位置
+layout(location = 1) in vec2 texcoord;  // テクスチャ座標
+
+// 出力データ
+// out vec3 Color;
+out vec2 Texcoord;  // フラグメントシェーダーに渡すテクスチャ座標
+
+void main(){
+    Texcoord = texcoord;    // 入力されたテクスチャ座標を出力変数に渡す
+    gl_Position = vec4(vertexPosition_modelspace, 1.0); // 頂点の位置をクリップ空間に設定
+}
